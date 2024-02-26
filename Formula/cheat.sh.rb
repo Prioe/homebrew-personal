857c987c9596b6f77ac8b2fecedd760bf90077c5
Michael Albertz <michael@prio.one>
2024-02-26 16:59:23 +0100

    fix cheat.sh tests

--- Formula/cheat.sh.rb
+++ Formula/cheat.sh.rb
@@ -15,6 +15,6 @@ class CheatSh < Formula
   end
 
   test do
-    assert_match("The only cheat sheet you need", shell_output("#{bin}/cht.sh --version"))
+    assert_match("To access a cheat sheet you can simply issue a plain HTTP or HTTPS request", shell_output("#{bin}/cht.sh :intro"))
   end
 end

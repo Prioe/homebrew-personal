7c590a21081a95076027c45b5eb3e4d0bf9d2199
Michael Albertz <michael@prio.one>
2024-02-26 16:06:52 +0000

    cheat.sh: add unstable-2024-02-26 bottle.

--- Formula/cheat.sh.rb
+++ Formula/cheat.sh.rb
@@ -6,6 +6,12 @@ class CheatSh < Formula
   sha256 "e676f372045bc185c82ac58a0f66fe29d16013251e41ab847764e80d697b88c1"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Prioe/homebrew-personal/releases/download/cheat.sh-unstable-2024-02-26"
+    sha256 cellar: :any_skip_relocation, ventura:      "7570f6a0e74731b8573dac1ba0c5d9ad014c0b683bd4a8a4957491658020d798"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8432de4eacc5c8e886c5f0f66013e758584c5843561e0a63a69eab1e3c70450b"
+  end
+
   def install
     bin.install "share/cht.sh.txt" => "cht.sh"
 
